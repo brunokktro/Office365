94f3b09df456f7e2523d39fb4f03ff1d8b4c2790
Bruno Lopes <37803210+brunokktro@users.noreply.github.com>
2018-04-18 23:20:39 -0300

    Delete Get-MailboxStatisticsinMB.ps1

--- Get-MailboxStatisticsinMB.ps1
+++
@@ -1,1 +0,0 @@
-﻿Get-MailboxStatistics | where {$_.ObjectClass –eq “Mailbox”} | Sort-Object TotalItemSize –Descending | ft @{label=”User”;expression={$_.DisplayName}},@{label=”Total Size (MB)”;expression={$_.TotalItemSize.Value.ToMB()}},@{label=”Items”;expression={$_.ItemCount}},@{label=”Storage Limit”;expression={$_.StorageLimitStatus}} -auto

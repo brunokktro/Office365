acb9d8f1fde64b51e17bebb596e8da34ddaebc71
Bruno Lopes <37803210+brunokktro@users.noreply.github.com>
2018-04-18 23:10:28 -0300

    Add files via upload

---
+++ Ex2013-PrepareAD.ps1
@@ -0,0 +1,21 @@
+﻿## Instalação do RSAT para comunicação com AD
+
+Install-WindowsFeature RSAT-ADDS
+Install-WindowsFeature RSAT-Clustering-CmdInterface
+
+## Apenas em casos de florestas legadas de Exchange já implantadas
+
+Setup /PrepareLegacyExchangePermissions
+
+
+## Novas florestas a partir daqui
+
+Setup /PrepareSchema /IAcceptExchangeServerLicenseTerms
+
+Setup /PrepareAD /OrganizationName:FIORELLA /IAcceptExchangeServerLicenseTerms
+
+Setup /PrepareAllDomains /IAcceptExchangeServerLicenseTerms
+
+Setup /mode:Install /r:CA,MB /IAcceptExchangeServerLicenseTerms /MdbName:MB01 /DbFilePath:D:\ExchData\MBData\MB01.edb /LogFolderPath:D:\ExchData\MBLogs
+
+

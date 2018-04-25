0078896c6359924d5a0bf6a1e571fcf30d5c49b2
Bruno Lopes <37803210+brunokktro@users.noreply.github.com>
2018-04-25 19:22:53 -0300

    Add files via upload

---
+++ Connect-Office365.ps1
@@ -0,0 +1,10 @@
+﻿$credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $credential
+## Import-Module SkypeOnlineConnector
+## $sfboSession = New-CsOnlineSession -Credential $credential
+## Import-PSSession $sfboSession
+$exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $credential -Authentication "Basic" -AllowRedirection
+Import-PSSession $exchangeSession -DisableNameChecking
+$ccSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection
+Import-PSSession $ccSession -Prefix cc
